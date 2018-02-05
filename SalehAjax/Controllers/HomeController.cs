d472769236075b22937a9cb8400e50141a2b4bd9
Saleh Osman <saleh1.a.osman@gmail.com>
2018-02-05 16:06:23 +0100

    added ajax for create and search

--- SalehAjax/Controllers/HomeController.cs
+++ SalehAjax/Controllers/HomeController.cs
@@ -15,33 +15,66 @@ namespace SalehAjax.Controllers
             return View(MyList.myList);
         }
 
-        //[HttpGet] to fix problem in searching method no need to strict this method as get or post because index method use partialindex by to way get and post
+        [HttpPost]/* we create new action for ajax to fitach each raw in list
+        not all list then we make it return to partial view */
+
+        public ActionResult _Table(string searchTxt = "", string City = "")
+        {
+            if (!string.IsNullOrEmpty(searchTxt))//is not null then go in
+            {
+                if (City == "city")
+                {
+                    return PartialView(MyList.myList.Where(x => x.City.ToLower().Contains(searchTxt.ToLower())).OrderBy(x => x.Name));
+                }
+                else //if (City != "city") because I have a default ="" upp 
+                 {
+                    return PartialView(MyList.myList.Where(x => x.Name.ToLower().Contains(searchTxt.ToLower())).OrderBy(x => x.City));
+                }
+            }
+            else// it is not null, it has another value
+            {
+                return PartialView(MyList.myList);//all list with out filtering 
+            }
+        }
+
+        //[HttpPost]
+        //public ActionResult Index(string searchTxt = "", string City = "")
+        //{
+        //    if (!string.IsNullOrEmpty(searchTxt))//is not null then go in
+        //    {
+        //        if (City == "city")
+        //        {
+        //            return View(MyList.myList.Where(x => x.City.ToLower().Contains(searchTxt.ToLower())).OrderBy(x => x.Name));
+        //        }
+        //        else if (City != "city")
+        //        {
+        //            return View(MyList.myList.Where(x => x.Name.ToLower().Contains(searchTxt.ToLower())).OrderBy(x => x.City));
+        //        }
+        //    }
+        //    else// is null
+        //    {
+        //        return View(MyList.myList);//all list with out filtering 
+        //    }
+        //    return View("Index");
+        //}
+
+        /*[HttpGet] we use this method as assistant to baind with RenderAction to display list of search
+         *to fix problem in searching method no need to strict this method as get or post because index method use partialindex by to way get and post*/
+
         public ActionResult PrtialIndex(int id)
         {
             Person person = MyList.myList.SingleOrDefault(p => p.Id == id);
             return PartialView("_PartialPerson", person);
         }
 
-        [HttpPost]
-        public ActionResult Index(string searchTxt = "", string City = "")
-        {
-            if (!string.IsNullOrEmpty(searchTxt))//is not null then go in
-            {
-                if (City == "city")
-                {
-                    return View(MyList.myList.Where(x => x.City.ToLower().Contains(searchTxt.ToLower())).OrderBy(x => x.Name));
-                }
-                else if (City != "city")
-                {
-                    return View(MyList.myList.Where(x => x.Name.ToLower().Contains(searchTxt.ToLower())).OrderBy(x => x.City));
-                }
-            }
-            else// is null
-            {
-                return View(MyList.myList);//all list with out filtering 
-            }
-            return View("Index");
-        }
+        //public ActionResult Search(string src, string City)
+        //{
+        //    Person SrcPerson = new Person();
+        //    if (City == "city")
+        //    {
+        //    }
+        //    return View();
+        //}
 
         [HttpGet]
         public ActionResult Create()
@@ -49,14 +82,20 @@ namespace SalehAjax.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ActionResult _Create()
+        {
+            return PartialView();
+        }
+
         [HttpPost]
-        public ActionResult Create(Person newPerson)
+        public ActionResult _Create(Person newPerson)
         {
             newPerson.Id = MyList.myList.Last().Id + 1;
 
             MyList.myList.Add(newPerson);
 
-            return RedirectToAction("Index");
+            return PartialView("_PartialPerson", newPerson);
         }
 
         [HttpGet]
@@ -109,15 +148,6 @@ namespace SalehAjax.Controllers
             return RedirectToAction("Index");
         }
 
-        public ActionResult Search(string src, string City)
-        {
-            Person SrcPerson = new Person();
-            if (City == "city")
-            {
-            }
-
-            return View();
-        }
 
     }
 }
